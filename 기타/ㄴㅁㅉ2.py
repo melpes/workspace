d99a40f320cc014cc342ee42461547812caa0ca8
Lev-melpes <91787650+Lev-melpes@users.noreply.github.com>
2022-07-05 21:40:47 +0900

    f

--- 기타/ㄴㅁㅉ2.py
+++ 기타/ㄴㅁㅉ2.py
@@ -1,7 +1,7 @@
-import random
+import random, re, math
 
 n = int(input())
-maximum = 8
+maximum = 6
 
 numbers = [i for i in range(1, maximum + 1)]
 operators = ['*', '/', '+', '-', '^']
@@ -36,29 +36,175 @@ def set_brackets(length) -> list:
     return bracket_value
 
 # 사칙연산+제곱 처리
-def set_basic_operator(length) -> list:
+def set_basic_operator(length, operators) -> list:
     basic_operator = [random.choice(operators) for _ in range(length - 1)]
     return basic_operator
 
 # 루트, 팩토리얼 처리
 def set_rf_operator(length, bracket_value):
     i = sum(filter(lambda x:x>0, bracket_value)) + length
-    root_operator = [random.randint(0, 1) for _ in range(i)]
-    facto_operator = [random.randint(0, 1) for _ in range(i)]
+    root_operator = [random.randint(0, 0) for _ in range(i)]
+    facto_operator = [random.randint(0, 0) for _ in range(i)]
     return root_operator, facto_operator
 
 # 최종 식 문자열로 표기
-def assemble_expression(length, nums, bracket_value):
-    ans = ''
+def assemble_expression(length, nums, bracket_value, basic_operator, root_operator, facto_operator):
+    ans = []
 
     for i in range(length):
         if bracket_value[i] > 0:
-            ans += '(' * bracket_value[i]
-            ans += str(nums[i])
+            ans += ['('] * bracket_value[i]
+            ans += list(str(nums[i]))
         else:
-            ans += str(nums[i])
-            ans += ')' * -bracket_value[i]
+            ans += list(str(nums[i]))
+            ans += [')'] * -bracket_value[i]
+
+    k = -1
+    for i in range(len(ans)-1, -1, -1):
+        if re.match(r"[0-9]", ans[i]):
+            j = 1
+            while True:
+                if ans[i-j] != '(':
+                    break
+                if i-j-1 == -1:
+                    break
+                j += 1
+
+            ans.insert(i-j+1, basic_operator[k])
+            if -k == len(basic_operator) :
+                break
+            k -= 1
+
+    posi_brac_index, nega_brac_index = [], []
+    for i in range(len(ans)):
+        if ans[i] == '(' or re.match(r"[0-9]", ans[i]):
+            posi_brac_index.append(i)
+
+    for i in range(len(posi_brac_index) - 1, -1, -1):
+        if root_operator[i]:
+            ans.insert(posi_brac_index[i], "r")
+    
+    for i in range(len(ans)):
+        if ans[i] == ')' or re.match(r"[0-9]", ans[i]):
+            nega_brac_index.append(i)
+    
+    for i in range(len(nega_brac_index) - 1, -1, -1):
+        if facto_operator[i]:
+            ans.insert(nega_brac_index[i] + 1, "!")
+    
     return ans
+
+def to_expression(lst):
+    expression = ''
+    for i in lst:
+        expression += str(i)
+
+    return expression
+
+def calculate(exp_list):
+    keep = True
+    while keep:
+        new_brackests = [0 for i in range(len(exp_list))]
+        for i in range(len(exp_list)):
+            if exp_list[i] == '(':
+                new_brackests[i] += 1
+            elif exp_list[i] == ')':
+                new_brackests[i] -= 1
+
+
+        posi_list = [i for i, v in enumerate(new_brackests) if v == 1]
+
+        if len(posi_list) == 0:
+            break
+        
+        starter = 0
+        ender = new_brackests.index(-1)
+        for i in posi_list:
+            if i >= ender:
+                break
+            starter = i
+
+        sample = [exp_list.pop(i) for i in range(ender-1, starter, -1)]
+        sample.reverse()
+
+        cal = 0
+        for i in range(len(sample)):
+            if type(sample[i]) != str:
+                continue
+            if re.match(r"[\*/\+\-\^]", sample[i]):
+                cal = i
+            if re.match(r"[0-9]", sample[i]):
+                sample[i] = int(sample[i])
+
+        joint = [sample[:cal], sample[cal+1:]]
+
+        res = [0, 0]
+        for i in range(len(joint)):
+            if 'r' in joint[i]:
+                if joint[i][1] < 0:
+                    print("root error")
+                    keep = False
+                    break
+                res[i] = joint[i][1] ** 0.5
+            else:
+                res[i] = joint[i][0]
+
+            if '!' in joint[i]:
+                try:
+                    res[i] = math.factorial(res[i])
+                except:
+                    # print("facto error")
+                    keep = False
+                    break
+
+        res.append(sample[cal])
+
+        ans = 0
+        try:
+            if res[0] > 1e3 or res[1] > 1e3:
+                keep = False
+                break
+        except:
+            keep = False
+            break
+        try:
+            if res[-1] == "*":
+                ans = res[0] * res[1]
+            elif res[-1] == "/":
+                ans = res[0] / res[1]
+            elif res[-1] == "+":
+                ans = res[0] + res[1]
+            elif res[-1] == "-":
+                ans = res[0] - res[1]
+            elif res[-1] == "^":
+                ans = res[0] ** res[1]
+        except:
+            # print("cal error")
+            keep = False
+            break
+
+        try:
+            if ans > 1e5:
+                keep = False
+                break
+        except:
+            keep = False
+            break
+        
+        exp_list.insert(starter+1, ans)
+        del exp_list[starter]
+        del exp_list[starter+1]
+
+    if '!' in exp_list:
+        try:
+            exp_list[-2] = math.factorial(exp_list[-2])
+            del exp_list[-1]
+        except:
+            keep = False
+    
+    return exp_list, keep
+        
+    
 
 length = len(numbers)
 count = 0
@@ -67,10 +213,21 @@ while True:
 
     nums = set_nums(numbers=numbers)
     bracket_value = set_brackets(length=length)
-    basic_operator = set_basic_operator(length=length)
+    basic_operator = set_basic_operator(length, operators)
+    root_operator, facto_operator = set_rf_operator(length, bracket_value)
 
-    expression = assemble_expression(length=length, nums=nums, bracket_value=bracket_value)
+    exp_list = assemble_expression(length, nums, bracket_value, basic_operator, root_operator, facto_operator)
 
-    print(expression)
-    if count == 10:
-        break
+    expression = to_expression(exp_list)
+    result, keep = calculate(exp_list)
+    ans = to_expression(result)
+    
+    if keep:
+        print(count, ans)
+
+    try:
+        if abs(float(ans)) == n:
+            print(expression)
+            break
+    except:
+        pass
