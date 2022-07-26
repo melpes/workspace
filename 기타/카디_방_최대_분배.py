eac1062057fc346820bc9266cd8d10072a1c6046
Lev-melpes <91787650+Lev-melpes@users.noreply.github.com>
2022-07-26 16:49:41 +0900

    Node.value에 int 대신 Unit값으로 대체 과정. 오류 있음

--- 기타/카디_방_최대_분배.py
+++ 기타/카디_방_최대_분배.py
@@ -1,9 +1,10 @@
+import random
 import numpy as np
 
 NODES_SIZE = {'x':3, 'y':3}
 
 def main() -> None:
-    nodes = fill_new_nodes(NODES_SIZE, nodes)
+    nodes = fill_new_nodes(NODES_SIZE)
 
     set_nodes_dir(nodes)
     
@@ -14,8 +15,39 @@ def main() -> None:
         move(nodes)
         input()
 
+class Node:
+    def __init__(self, coor : tuple) -> None:
+        self.to_dir = {}
+        self.node_num = coor
+        self._values = list()
+        self.pre_value = list()
+    
+    @property
+    def value(self):
+        self.check()
+        return self._value
+
+    @value.setter
+    def value(self, v):
+        self._value = v
+
+    def check(self):
+        assert sum(self.to_dir.values()) == 1, "\
+\   \   NoneConservation : sum of Node.to_dir.keys() is not 1."
+            
+class Unit:
+    def __init__(self) -> None:
+        self.last_visited_node = None
+
+    def cal_coor(self, to_dir) -> tuple:
+        key = random.random()
+        for coor, percent in to_dir.items():
+            key -= percent
+            if key <= 0:
+                return coor
+
 def fill_new_nodes(nodes_size) -> list:
-    nodes = np.full((nodes_size.keys()), None)
+    nodes = np.full((list(nodes_size.values())), None)
     for x in range(nodes_size['x']):
         for y in range(nodes_size['y']):
             nodes[x, y] = Node((x, y))
@@ -42,56 +74,30 @@ def set_nodes_dir(nodes) -> None:
     nodes[2, 2].to_dir[2-1, 2-1] = 1
 
 def set_nodes_value(nodes) -> None:
-    nodes[0, 1].value = 1
-    nodes[2, 1].value = 1
+    nodes[0, 1].value.append(Unit())
+    nodes[2, 1].value.append(Unit())
 
 def print_node(node : np) -> None:
     for y in range(node.shape[1]):
         for x in range(node.shape[0]):
-            print(node[x][y].value, end="\t")
+            print(len(node[x][y].value), end="\t")
         print()
 
 def move(nodes) -> None:
     dim = nodes.shape
     for x in range(dim[0]):
         for y in range(dim[1]):
-            nodes[x, y].pre_value = 0
+            nodes[x, y].pre_value = list()
 
     for x in range(dim[0]):
         for y in range(dim[1]):
-            for coor, percent in nodes[x, y].to_dir.items():
-                nodes[coor].pre_value += nodes[x, y].value * percent
+            for unit in nodes[x, y].value:
+                coor = unit.cal_coor(nodes[x, y].to_dir)
+                nodes[coor].pre_value = unit
 
     for x in range(dim[0]):
         for y in range(dim[1]):
-            nodes[x, y].value = round(nodes[x, y].pre_value, 3)
-
-class Node:
-    def __init__(self, coor : tuple) -> None:
-        self.to_dir = {}
-        self.node_num = coor
-        self._value = 0
-        self.pre_value = 0
-    
-    @property
-    def value(self):
-        self.check()
-        return self._value
-
-    @value.setter
-    def value(self, v):
-        self._value = v
-
-    def check(self):
-        assert sum(self.to_dir.values()) == 1, "\
-    NoneConservation : sum of Node.to_dir.keys() is not 1."
-            
-
-
-
-
-            
-    
+            nodes[x, y].value = nodes[x, y].pre_value
 
 if __name__ == "__main__":
     main()
