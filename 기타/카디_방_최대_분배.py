a8210e41c5cc3c0272b09006fde4d4ebd128d30d
Lev-melpes <91787650+Lev-melpes@users.noreply.github.com>
2022-07-26 13:53:13 +0900

    메인 함수에 변경시 중복될만한 점들 고치고 바뀔만한 점들 강조선언

--- 기타/카디_방_최대_분배.py
+++ 기타/카디_방_최대_분배.py
@@ -1,10 +1,9 @@
 import numpy as np
 
+NODES_SIZE = {'x':3, 'y':3}
+
 def main() -> None:
-    nodes = np.full((3, 3), None)
-    for x in range(3):
-        for y in range(3):
-            nodes[x, y] = Node((x, y))
+    nodes = fill_new_nodes(NODES_SIZE, nodes)
 
     set_nodes_dir(nodes)
     
@@ -15,11 +14,57 @@ def main() -> None:
         move(nodes)
         input()
 
+def fill_new_nodes(nodes_size) -> list:
+    nodes = np.full((nodes_size.keys()), None)
+    for x in range(nodes_size['x']):
+        for y in range(nodes_size['y']):
+            nodes[x, y] = Node((x, y))
+    return nodes
+
+def set_nodes_dir(nodes) -> None:
+    nodes[0, 0].to_dir[0+1, 0+1] = 1
+
+    nodes[1, 0].to_dir[1-1, 0] = 1
+    
+    nodes[2, 0].to_dir[2-1, 0] = 1
+
+    nodes[0, 1].to_dir[0, 1+1] = 1
+    
+
+    nodes[1, 1].to_dir[1, 1] = 1
+    
+    nodes[2, 1].to_dir[2, 1-1] = 1
+    
+    nodes[0, 2].to_dir[0+1, 2] = 1
+
+    nodes[1, 2].to_dir[1+1, 2] = 1
+
+    nodes[2, 2].to_dir[2-1, 2-1] = 1
+
+def set_nodes_value(nodes) -> None:
+    nodes[0, 1].value = 1
+    nodes[2, 1].value = 1
+
 def print_node(node : np) -> None:
     for y in range(node.shape[1]):
         for x in range(node.shape[0]):
             print(node[x][y].value, end="\t")
         print()
+
+def move(nodes) -> None:
+    dim = nodes.shape
+    for x in range(dim[0]):
+        for y in range(dim[1]):
+            nodes[x, y].pre_value = 0
+
+    for x in range(dim[0]):
+        for y in range(dim[1]):
+            for coor, percent in nodes[x, y].to_dir.items():
+                nodes[coor].pre_value += nodes[x, y].value * percent
+
+    for x in range(dim[0]):
+        for y in range(dim[1]):
+            nodes[x, y].value = round(nodes[x, y].pre_value, 3)
 
 class NoneConservation(Exception):
     pass
@@ -45,45 +90,10 @@ class Node:
             print("todir", self.to_dir.values())
             raise NoneConservation # assert로 바꾸기
             
-def set_nodes_dir(nodes) -> None:
-    nodes[0, 0].to_dir[0+1, 0+1] = 1
-
-    nodes[1, 0].to_dir[1-1, 0] = 1
-    
-    nodes[2, 0].to_dir[2-1, 0] = 1
-
-    nodes[0, 1].to_dir[0, 1+1] = 1
-    
-
-    nodes[1, 1].to_dir[1, 1] = 1
-    
-    nodes[2, 1].to_dir[2, 1-1] = 1
-    
-    nodes[0, 2].to_dir[0+1, 2] = 1
-
-    nodes[1, 2].to_dir[1+1, 2] = 1
-
-    nodes[2, 2].to_dir[2-1, 2-1] = 1
-
-def set_nodes_value(nodes):
-    nodes[0, 1].value = 1
-    nodes[2, 1].value = 1
 
 
-def move(nodes):
-    dim = nodes.shape
-    for x in range(dim[0]):
-        for y in range(dim[1]):
-            nodes[x, y].pre_value = 0
 
-    for x in range(dim[0]):
-        for y in range(dim[1]):
-            for coor, percent in nodes[x, y].to_dir.items():
-                nodes[coor].pre_value += nodes[x, y].value * percent
 
-    for x in range(dim[0]):
-        for y in range(dim[1]):
-            nodes[x, y].value = round(nodes[x, y].pre_value, 3)
             
     
 
