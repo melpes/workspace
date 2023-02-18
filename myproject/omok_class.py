f0dfd1b4d7790430bd23a8a09b08a1245dad66ef
melpes <vegakangtaehui@gmail.com>
2023-02-18 23:09:38 +0900

    마킹 시스템 완성. 마킹 시스템 시각화 진행중. Board의 print에 덮어씌울 예정

--- myproject/omok_class.py
+++ myproject/omok_class.py
@@ -9,7 +9,14 @@ def main() -> None:
     board.check_winner()
 
     ai = Ai(board)
-    ai.marking(np.array([0, 0, 1, -1, 1, 0, 1, 0, 1, 0]))
+    # ai.marking(np.array([0, 0, 1, -1, 1, 0, 1, 0, 1, 0]))
+    ai.judgment()
+    print(np.where(ai.full_markers >= 1))
+    for i in range(len(np.where(ai.full_markers >= 1)[0])):
+        print(np.where(ai.full_markers >= 1)[0][i] % 10, np.where(ai.full_markers >= 1)[1][i] % 10)
+    # for i in ['x', 'y', 'xy', '-xy']:
+    #     print("loc", ai.full_markers.loc[:, (i, 'a')], "?", sep="\n")
+    ai.print()
 
 
 class Board:
@@ -93,31 +100,74 @@ class Ai:
 
     def __init__(self, board) -> None:
         self.board : Board = board
-    
-    def judgment(self):
+
+    def print(self):
+        mark_dict = {
+            4 : 'a',
+            3 : 'b',
+            2 : 'c',
+            1 : 'd'
+        }
+        visualized = self.board.stone_info.copy().astype(str)
+        visualized[np.where(visualized == '-1')] = '○'
+        visualized[np.where(visualized == '1')] = '●'
+        visualized[np.where(visualized == '0')] = ' '
+        # for i in range(1, 5):
+        #     print(np.where(self.full_markers.loc[:, ('a')] >= 1))
+        #     visualized[np.where(self.full_markers[:, (:, 'a')] >= 1)] = str(i + 1)
+        for y in range(self.board.size_of_board):
+            for x in range(self.board.size_of_board):
+                print(visualized[x, y], end=' ')
+            print()
+
+    def judgment(self) -> None:
         # 한 줄씩 따로 생각한다.
         # 각 줄마다 alpha, beta, charlie, delta 및 각각의 마킹에 원형, blanked, blocked의 변형 위치를 기록
+        mark_dict = {
+            4 : 'a',
+            3 : 'b',
+            2 : 'c',
+            1 : 'd'
+        }
 
-        for i, line in enumerate(self.line_range()):
+        index = pd.MultiIndex.from_product([[i for i in range(self.board.size_of_board)]] * 2, names=['x', 'y'])
+        columns = pd.MultiIndex.from_product([['x', 'y', 'xy', '-xy'], ['a', 'b', 'c', 'd', "a-", "b-", "c-", "d-", "is blocked"]])
+        full_markers = pd.DataFrame(0, index=index, columns=columns)
+        print(full_markers)
+
+        for i, (line, dir) in enumerate(self.line_range()):
             i %= self.board.size_of_board
             if line.size == 0:
                 continue
-            self.marking(line)
-            # for j, info in enumerate(line_info):
-            #     if dir == 'x':
-            #         index = i, j
-            #     elif dir == 'y':
-            #         index = j, i
-            #     elif dir == 'xy1':
-            #         index = j, size+j - (i+1)
-            #     elif dir == 'xy2':
-            #         index = i+j+1, j
-            #     elif dir == '-xy1':
-            #         index = j, i-j
-            #     elif dir == '-xy2':
-            #         index = i+1, size - (j+1)
+            line_markers = self.marking(line)
 
-    def marking(self, line):
+            for j in range(line_markers.shape[0]):
+                match dir:
+                    case 'x':
+                        index = (i, j)
+                    case 'y':
+                        index = (j, i)
+                    case 'xy1':
+                        index = (j, self.board.size_of_board + j - (i + 1))
+                        dir = 'xy'
+                    case 'xy2':
+                        index = (i + j + 1, j)
+                        dir = 'xy'
+                    case '-xy1':
+                        index = (j, i - j)
+                        dir = '-xy'
+                    case '-xy2':
+                        index = (i + 1, self.board.size_of_board - (j + 1))
+                        dir = '-xy'
+                if line_markers.loc[j, "score level"] > 4 or line_markers.loc[j, "score level"] == 0:
+                    continue
+                mark = mark_dict[line_markers.loc[j, "score level"]] + ('-' if line_markers.loc[j, "empty level"] >= 1 else '')
+                full_markers.loc[index, (dir, mark)] += 1
+                if line_markers.loc[j, "is blocked"] == True:
+                    full_markers.loc[index, (dir, mark)] = 1
+        self.full_markers = full_markers
+
+    def marking(self, line) -> pd.DataFrame:
         marker = pd.Series({i:0 for i in range(line.size)})
         df = {
             "info" : line,
@@ -126,15 +176,11 @@ class Ai:
             "is blocked" : False
         }
         line_markers = pd.DataFrame(df)
-        print(line)
-        print(line_markers)
 
         for i in range(line_markers.shape[0]):
             if line_markers.loc[i, "info"] == -1 * self.board.turn:
                 for dir in [-1, 1]:
                     self.spreading_blocked(line_markers, i, dir)
-        print("marking is blocekd")
-        print(line_markers)
 
         stack = 0
         for i in range(line_markers.shape[0]):
@@ -142,13 +188,10 @@ class Ai:
                 stack += 1
             elif stack != 0:
                 is_blocked = line_markers.loc[i-1, "is blocked"]
-                print(i-1, is_blocked)
                 self.spreading_level(line_markers, i, stack, is_blocked)
                 stack = 0
-        print("marking levels")
-        print(line_markers)
 
-
+        return line_markers
 
     def spreading_level(self, line_markers, i, stack, is_blocked, dir=0, blank_entry=0):
         # self.turn에 해당하는 칸 앞뒤 두칸씩 score level을 stack만큼 올립니다.
@@ -174,7 +217,6 @@ class Ai:
             line_markers.loc[idx, "empty level"] += blank
             if is_blocked == True:
                 line_markers.loc[idx, "is blocked"] = True
-            print(i, stack, is_blocked)
 
         for blank in range(2):
             idx = (i - 1 - stack) - blank
@@ -193,7 +235,6 @@ class Ai:
             line_markers.loc[idx, "score level"] += stack
             if is_blocked == True:
                 line_markers.loc[idx, "is blocked"] = True
-            print(i - 1 - stack, blank_entry, is_blocked)
 
     def spreading_blocked(self, line_markers, i, dir):
         if line_markers.loc[i + dir, "info"] != self.board.turn:
@@ -204,22 +245,22 @@ class Ai:
     def line_range(self) -> list:
         size = self.board.size_of_board
         for v in self.board.stone_info:
-            yield v
+            yield v, 'x'
         
         for v in self.board.stone_info.T:
-            yield v
+            yield v, 'y'
 
         for i in np.arange(0, size):
-            yield self.board.stone_info[0:1+i, size-1-i:size].diagonal()
+            yield self.board.stone_info[0:1+i, size-1-i:size].diagonal(), "xy1"
         for i in np.arange(size-2, -1, -1):
-            yield self.board.stone_info.T[0:1+i, size-1-i:size].diagonal()
-        yield np.array([])
+            yield self.board.stone_info.T[0:1+i, size-1-i:size].diagonal(), "xy2"
+        yield np.array([]), ''
             
         for i in np.arange(0, size):
-            yield self.board.stone_info[:,::-1][0:1+i, size-1-i:size].diagonal()
+            yield self.board.stone_info[:,::-1][0:1+i, size-1-i:size].diagonal(), "-xy1"
         for i in np.arange(size-2, -1, -1):
-            yield self.board.stone_info.T[::-1][0:1+i, size-1-i:size].diagonal()
-        yield np.array([])
+            yield self.board.stone_info.T[::-1][0:1+i, size-1-i:size].diagonal(), "-xy2"
+        yield np.array([]), ''
 
 if __name__ == "__main__":
     main()
